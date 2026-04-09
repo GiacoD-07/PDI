e218d0f01bbf65c8663128ecf390cfa62ae25346
GiacoD-07 <giacomelli.e.dylan@gmail.com>
2026-04-09 17:03:08 -0300

    Add files via upload

---
+++ tp-2/ejemplos.php
@@ -0,0 +1,10 @@
+<?php
+function esMayor(int $a,int $b){
+   //$precio=$producto + $iva;
+  //$r=!$a && $b;
+}
+function renderizar($estaLogueado,$esAdmin){
+   // TienePermisos= estaLogueado&&esAdmin;
+}
+
+?>
